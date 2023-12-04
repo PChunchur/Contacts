a805965579bc62d7f628b67b594ffe3a0e315119
Pranav Chunchur <112144655+PChunchur@users.noreply.github.com>
2023-12-04 12:57:51 +0530

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Wisdom.Resources.Views;
+
+namespace Wisdom
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute(nameof(ContactsPage), typeof(ContactsPage));
+            Routing.RegisterRoute(nameof(AddContactsPage),typeof(AddContactsPage));
+            Routing.RegisterRoute(nameof(EditContactsPage), typeof(EditContactsPage));
+
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Wisdom
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
